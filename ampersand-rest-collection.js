232855df850a69fa6234aa7d5b5e7621423e9876
Philip Roberts <phil@andyet.net>
2014-10-31 14:18:05 +0000

    Add ampersand-version

--- ampersand-rest-collection.js
+++ ampersand-rest-collection.js
@@ -1,3 +1,4 @@
+/*$AMPERSAND_VERSION*/
 var Collection = require('ampersand-collection');
 var underscoreMixin = require('ampersand-collection-underscore-mixin');
 var restMixins = require('ampersand-collection-rest-mixin');
